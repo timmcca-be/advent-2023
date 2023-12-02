ddbbcf5478ddb645764423dd90b36b24b664dda0
Tim McCabe <timmccabe@khanacademy.org>
2023-12-02 14:44:00 -0500

    Solve day 2 step 1

---
+++ day-2/src/main.rs
@@ -0,0 +1,36 @@
+use clap::Parser;
+use regex::Regex;
+
+#[derive(Parser)]
+struct Args {
+    input_path: String,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let content = std::fs::read_to_string(&args.input_path).expect("could not read file");
+
+    let impossible_pattern: Regex =
+        Regex::new(r"13 red|14 (?:red|green)|(?:1[5-9]|[2-9]\d|\d{3}) (?:red|green|blue)")
+            .expect("could not parse impossible game pattern");
+    let game_number_pattern: Regex = Regex::new(r"\d+").expect("could not parse game number");
+
+    let mut sum = 0;
+    for line in content.lines() {
+        if impossible_pattern.is_match(line) {
+            continue;
+        }
+
+        let game_number = game_number_pattern
+            .find(line)
+            .expect("could not get game number")
+            .as_str()
+            .parse::<i32>()
+            .expect("could not parse game number");
+
+        sum += game_number;
+    }
+
+    println!("sum: {}", sum);
+}
