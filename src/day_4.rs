1ae522d5981bb9eb4d1ce0cebeff502742215547
Tim McCabe <timmccabe@khanacademy.org>
2023-12-04 12:04:40 -0500

    Solve day 4 step 2

--- src/day_4.rs
+++ src/day_4.rs
@@ -1,6 +1,6 @@
 use lazy_static::lazy_static;
 use regex::Regex;
-use std::collections::HashSet;
+use std::collections::{HashSet, LinkedList};
 
 lazy_static! {
     static ref TOKEN_PATTERN: Regex = Regex::new(r"\d+|\|").unwrap();
@@ -38,4 +38,50 @@ pub fn step_1(content: &str) {
     println!("sum: {}", sum);
 }
 
-pub fn step_2(_content: &str) {}
+pub fn step_2(content: &str) {
+    let mut num_cards = 0;
+    let mut extra_copies_counts: LinkedList<i32> = LinkedList::new();
+
+    for line in content.lines() {
+        let mut token_iterator = TOKEN_PATTERN.find_iter(line);
+        // skip the card number
+        token_iterator.next();
+
+        let num_extra_copies = match extra_copies_counts.pop_front() {
+            Some(value) => value,
+            None => 0,
+        };
+        let multiplier = num_extra_copies + 1;
+        num_cards += multiplier;
+
+        let mut winning_numbers: HashSet<&str> = HashSet::new();
+        while let Some(m) = token_iterator.next() {
+            let token = m.as_str();
+            if token == "|" {
+                break;
+            }
+            winning_numbers.insert(token);
+        }
+
+        let mut cards_earned = 0;
+        while let Some(m) = token_iterator.next() {
+            if winning_numbers.contains(m.as_str()) {
+                cards_earned += 1;
+            }
+        }
+
+        for extra_copies in extra_copies_counts.iter_mut() {
+            if cards_earned == 0 {
+                break;
+            }
+
+            cards_earned -= 1;
+            *extra_copies += multiplier;
+        }
+        for _ in 0..cards_earned {
+            extra_copies_counts.push_back(multiplier);
+        }
+    }
+
+    println!("number of cards: {}", num_cards);
+}
