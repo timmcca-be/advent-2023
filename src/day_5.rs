b9a1fc1db5b31b388fa4414cc6a769b98f8ff781
Tim McCabe <timmccabe@khanacademy.org>
2023-12-05 11:37:05 -0500

    Solve day 5 step 2

--- src/day_5.rs
+++ src/day_5.rs
@@ -80,4 +80,365 @@ pub fn step_1<'a>(lines: impl IntoIterator<Item = String>) {
     println!("lowest location: {}", values.iter().min().unwrap());
 }
 
-pub fn step_2(_lines: impl IntoIterator<Item = String>) {}
+#[derive(Copy, Clone, PartialEq, Debug)]
+struct Range {
+    start: i64,
+    length: i64,
+}
+
+impl Range {
+    fn split(&self, other: Range) -> (Option<Range>, Option<Range>, Option<Range>) {
+        let bottom_length = num::clamp(other.start - self.start, 0, self.length);
+        let top_length = num::clamp(
+            self.start + self.length - (other.start + other.length),
+            0,
+            self.length,
+        );
+        let center_length = self.length - bottom_length - top_length;
+        return (
+            if bottom_length > 0 {
+                Some(Range {
+                    start: self.start,
+                    length: bottom_length,
+                })
+            } else {
+                None
+            },
+            if center_length > 0 {
+                Some(Range {
+                    start: std::cmp::max(self.start, other.start),
+                    length: center_length,
+                })
+            } else {
+                None
+            },
+            if top_length > 0 {
+                Some(Range {
+                    start: std::cmp::max(other.start + other.length, self.start),
+                    length: top_length,
+                })
+            } else {
+                None
+            },
+        );
+    }
+}
+
+pub fn step_2(lines: impl IntoIterator<Item = String>) {
+    let mut lines_iterator = lines.into_iter();
+    let seed_numbers: Vec<i64> = NUMBER_PATTERN
+        .find_iter(&lines_iterator.next().unwrap())
+        .map(|m| m.as_str().parse::<i64>().unwrap())
+        .collect();
+    let mut seeds_iterator = seed_numbers.iter();
+
+    let mut ranges: Vec<Range> = Vec::new();
+    while let Some(seed) = seeds_iterator.next() {
+        ranges.push(Range {
+            start: *seed,
+            length: *seeds_iterator.next().unwrap(),
+        });
+    }
+
+    // skip empty line
+    lines_iterator.next();
+
+    let range_mappings_iterator = RangeMappingsIterator {
+        lines: Box::new(lines_iterator),
+    };
+    for range_mappings in range_mappings_iterator {
+        let mut updated_ranges: Vec<Range> = Vec::new();
+        for range in &ranges {
+            let mut non_updated_ranges: Vec<Range> = Vec::new();
+            non_updated_ranges.push(*range);
+            for range_mapping in &range_mappings {
+                let mapping_source_range = Range {
+                    start: range_mapping.source_start,
+                    length: range_mapping.range_length,
+                };
+                let mut new_non_updated_ranges: Vec<Range> = Vec::new();
+                for non_updated_range in &non_updated_ranges {
+                    let (bottom, center, top) = non_updated_range.split(mapping_source_range);
+                    if bottom != None {
+                        new_non_updated_ranges.push(bottom.unwrap());
+                    }
+                    if top != None {
+                        new_non_updated_ranges.push(top.unwrap());
+                    }
+                    if center != None {
+                        let center_range = center.unwrap();
+                        updated_ranges.push(Range {
+                            start: center_range.start - range_mapping.source_start
+                                + range_mapping.destination_start,
+                            length: center_range.length,
+                        })
+                    }
+                }
+                non_updated_ranges = new_non_updated_ranges;
+            }
+
+            updated_ranges = updated_ranges
+                .iter()
+                .chain(non_updated_ranges.iter())
+                .map(|range| *range)
+                .collect();
+        }
+
+        ranges = updated_ranges;
+    }
+
+    println!(
+        "lowest location: {}",
+        ranges.iter().map(|range| range.start).min().unwrap()
+    );
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_split_other_below() {
+        // arrange
+        let target = Range {
+            start: 10,
+            length: 10,
+        };
+        let splitter = Range {
+            start: 0,
+            length: 8,
+        };
+
+        // act
+        let result = target.split(splitter);
+
+        // assert
+        assert_eq!(
+            result,
+            (
+                None,
+                None,
+                Some(Range {
+                    start: 10,
+                    length: 10,
+                })
+            )
+        )
+    }
+
+    #[test]
+    fn test_split_other_above() {
+        // arrange
+        let target = Range {
+            start: 0,
+            length: 10,
+        };
+        let splitter = Range {
+            start: 12,
+            length: 10,
+        };
+
+        // act
+        let result = target.split(splitter);
+
+        // assert
+        assert_eq!(
+            result,
+            (
+                Some(Range {
+                    start: 0,
+                    length: 10,
+                }),
+                None,
+                None,
+            )
+        )
+    }
+
+    #[test]
+    fn test_split_other_grazes_below() {
+        // arrange
+        let target = Range {
+            start: 10,
+            length: 10,
+        };
+        let splitter = Range {
+            start: 0,
+            length: 10,
+        };
+
+        // act
+        let result = target.split(splitter);
+
+        // assert
+        assert_eq!(
+            result,
+            (
+                None,
+                None,
+                Some(Range {
+                    start: 10,
+                    length: 10,
+                })
+            )
+        )
+    }
+
+    #[test]
+    fn test_split_other_grazes_above() {
+        // arrange
+        let target = Range {
+            start: 0,
+            length: 10,
+        };
+        let splitter = Range {
+            start: 10,
+            length: 10,
+        };
+
+        // act
+        let result = target.split(splitter);
+
+        // assert
+        assert_eq!(
+            result,
+            (
+                Some(Range {
+                    start: 0,
+                    length: 10,
+                }),
+                None,
+                None,
+            )
+        )
+    }
+
+    #[test]
+    fn test_split_other_intersects_top() {
+        // arrange
+        let target = Range {
+            start: 0,
+            length: 10,
+        };
+        let splitter = Range {
+            start: 5,
+            length: 10,
+        };
+
+        // act
+        let result = target.split(splitter);
+
+        // assert
+        assert_eq!(
+            result,
+            (
+                Some(Range {
+                    start: 0,
+                    length: 5,
+                }),
+                Some(Range {
+                    start: 5,
+                    length: 5
+                }),
+                None,
+            )
+        )
+    }
+
+    #[test]
+    fn test_split_other_intersects_bottom() {
+        // arrange
+        let target = Range {
+            start: 10,
+            length: 10,
+        };
+        let splitter = Range {
+            start: 5,
+            length: 10,
+        };
+
+        // act
+        let result = target.split(splitter);
+
+        // assert
+        assert_eq!(
+            result,
+            (
+                None,
+                Some(Range {
+                    start: 10,
+                    length: 5
+                }),
+                Some(Range {
+                    start: 15,
+                    length: 5,
+                }),
+            )
+        )
+    }
+
+    #[test]
+    fn test_split_other_is_nested() {
+        // arrange
+        let target = Range {
+            start: 10,
+            length: 10,
+        };
+        let splitter = Range {
+            start: 12,
+            length: 5,
+        };
+
+        // act
+        let result = target.split(splitter);
+
+        // assert
+        assert_eq!(
+            result,
+            (
+                Some(Range {
+                    start: 10,
+                    length: 2,
+                }),
+                Some(Range {
+                    start: 12,
+                    length: 5
+                }),
+                Some(Range {
+                    start: 17,
+                    length: 3,
+                }),
+            )
+        )
+    }
+
+    #[test]
+    fn test_split_other_wraps() {
+        // arrange
+        let target = Range {
+            start: 12,
+            length: 5,
+        };
+        let splitter = Range {
+            start: 10,
+            length: 10,
+        };
+
+        // act
+        let result = target.split(splitter);
+
+        // assert
+        assert_eq!(
+            result,
+            (
+                None,
+                Some(Range {
+                    start: 12,
+                    length: 5
+                }),
+                None
+            )
+        )
+    }
+}
