4ebf42d9a5d004520f535c7710e762ff0676f800
Tim McCabe <timmccabe@khanacademy.org>
2023-12-07 11:16:34 -0500

    Solve day 7 step 2

--- src/day_7.rs
+++ src/day_7.rs
@@ -1,33 +1,35 @@
+use lazy_static::lazy_static;
 use std::collections::HashMap;
 use std::vec::Vec;
+
+lazy_static! {
+    static ref STEP_1_CARD_VALUES: HashMap<u8, i32> =
+        [b'2', b'3', b'4', b'5', b'6', b'7', b'8', b'9', b'T', b'J', b'Q', b'K', b'A']
+            .into_iter()
+            .enumerate()
+            .map(|(i, byte)| (byte, i as i32))
+            .collect();
+    static ref STEP_2_CARD_VALUES: HashMap<u8, i32> =
+        [b'J', b'2', b'3', b'4', b'5', b'6', b'7', b'8', b'9', b'T', b'Q', b'K', b'A']
+            .into_iter()
+            .enumerate()
+            .map(|(i, byte)| (byte, i as i32))
+            .collect();
+}
 
 struct Hand {
     value: i32,
     bid: i32,
 }
 
-fn evaluate_card(card: u8) -> i32 {
-    return match card {
-        b'2' => 0,
-        b'3' => 1,
-        b'4' => 2,
-        b'5' => 3,
-        b'6' => 4,
-        b'7' => 5,
-        b'8' => 6,
-        b'9' => 7,
-        b'T' => 8,
-        b'J' => 9,
-        b'Q' => 10,
-        b'K' => 11,
-        b'A' => 12,
-        _ => panic!("unexpected card"),
-    };
-}
-
-fn get_hand_type_value(hand: &str) -> i32 {
+fn get_hand_type_value(hand: &str, is_step_2: bool) -> i32 {
+    let mut wildcards = 0;
     let mut counts: HashMap<u8, i32> = HashMap::new();
     for byte in hand.bytes() {
+        if is_step_2 && byte == b'J' {
+            wildcards += 1;
+            continue;
+        }
         let old_count = match counts.get(&byte) {
             Some(count) => *count,
             None => 0,
@@ -41,32 +43,41 @@ fn get_hand_type_value(hand: &str) -> i32 {
         .collect::<Vec<i32>>();
     sorted_counts.sort_unstable_by(|a, b| b.cmp(a));
 
-    let is_five_of_kind = sorted_counts[0] == 5;
+    let highest = match sorted_counts.get(0) {
+        Some(count) => *count,
+        None => 0,
+    } + wildcards;
+    let second_highest = match sorted_counts.get(1) {
+        Some(count) => *count,
+        None => 0,
+    };
+
+    let is_five_of_kind = highest == 5;
     if is_five_of_kind {
         return 6;
     }
 
-    let is_four_of_kind = sorted_counts[0] == 4;
+    let is_four_of_kind = highest == 4;
     if is_four_of_kind {
         return 5;
     }
 
-    let is_full_house = sorted_counts[0] == 3 && sorted_counts[1] == 2;
+    let is_full_house = highest == 3 && second_highest == 2;
     if is_full_house {
         return 4;
     }
 
-    let is_three_of_kind = sorted_counts[0] == 3;
+    let is_three_of_kind = highest == 3;
     if is_three_of_kind {
         return 3;
     }
 
-    let is_two_pair = sorted_counts[0] == 2 && sorted_counts[1] == 2;
+    let is_two_pair = highest == 2 && second_highest == 2;
     if is_two_pair {
         return 2;
     }
 
-    let is_one_pair = sorted_counts[0] == 2;
+    let is_one_pair = highest == 2;
     if is_one_pair {
         return 1;
     }
@@ -77,17 +88,25 @@ fn get_hand_type_value(hand: &str) -> i32 {
 
 const CARDS_PER_SUIT: i32 = 13;
 
-fn evaluate_hand_value(hand: &str) -> i32 {
+fn evaluate_hand_value(hand: &str, is_step_2: bool) -> i32 {
     return hand
         .bytes()
-        .map(|byte| evaluate_card(byte))
-        .fold(get_hand_type_value(hand), |a, b| a * CARDS_PER_SUIT + b);
+        .map(|byte| {
+            if is_step_2 {
+                STEP_2_CARD_VALUES[&byte]
+            } else {
+                STEP_1_CARD_VALUES[&byte]
+            }
+        })
+        .fold(get_hand_type_value(hand, is_step_2), |a, b| {
+            a * CARDS_PER_SUIT + b
+        });
 }
 
-pub fn step_1(lines: impl Iterator<Item = String>) {
+fn execute(lines: impl Iterator<Item = String>, is_step_2: bool) {
     let mut hands = lines
         .map(|line| Hand {
-            value: evaluate_hand_value(&line[..5]),
+            value: evaluate_hand_value(&line[..5], is_step_2),
             bid: line[6..].parse::<i32>().unwrap(),
         })
         .collect::<Vec<Hand>>();
@@ -104,4 +123,10 @@ pub fn step_1(lines: impl Iterator<Item = String>) {
     println!("sum: {}", sum);
 }
 
-pub fn step_2(_lines: impl Iterator<Item = String>) {}
+pub fn step_1(lines: impl Iterator<Item = String>) {
+    execute(lines, false);
+}
+
+pub fn step_2(lines: impl Iterator<Item = String>) {
+    execute(lines, true);
+}
