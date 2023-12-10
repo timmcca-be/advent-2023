6ec36cd65587ba8cc9a5e803b160a927aeb1cebc
Tim McCabe <timmccabe@khanacademy.org>
2023-12-10 09:34:28 -0500

    Simplify and optimize

--- src/day_9.rs
+++ src/day_9.rs
@@ -9,24 +9,15 @@ pub fn step_1(lines: impl Iterator<Item = String>) {
             .map(|string| string.parse::<i32>().unwrap())
             .collect();
 
-        let mut start: i32 = 0;
-        while values[(start as usize)..].iter().any(|value| *value != 0) {
-            start += 1;
-            for i in ((start as usize)..values.len()).rev() {
-                values[i] -= values[i - 1];
+        let mut end = values.len();
+        while values[0..end].iter().any(|value| *value != 0) {
+            end -= 1;
+            for j in 0..end {
+                values[j] = values[j + 1] - values[j];
             }
         }
 
-        let mut result = 0;
-        while start >= 0 {
-            for i in (start as usize)..(values.len() - 1) {
-                values[i + 1] += values[i];
-            }
-            result += values[values.len() - 1];
-            start -= 1;
-        }
-
-        sum += result;
+        sum += values[end..].iter().fold(0, |a, b| a + b);
     }
 
     println!("sum: {}", sum);
@@ -41,24 +32,15 @@ pub fn step_2(lines: impl Iterator<Item = String>) {
             .map(|string| string.parse::<i32>().unwrap())
             .collect();
 
-        let mut end = values.len();
-        while values[0..end].iter().any(|value| *value != 0) {
-            end -= 1;
-            for j in 0..end {
-                values[j] = values[j + 1] - values[j];
+        let mut start = 0;
+        while values[start..].iter().any(|value| *value != 0) {
+            start += 1;
+            for i in (start..values.len()).rev() {
+                values[i] -= values[i - 1];
             }
         }
 
-        let mut result = 0;
-        while end < values.len() {
-            for i in (0..end).rev() {
-                values[i] = values[i + 1] - values[i];
-            }
-            result = values[0] - result;
-            end += 1;
-        }
-
-        sum += result;
+        sum += values[0..start].iter().rev().fold(0, |a, b| b - a);
     }
 
     println!("sum: {}", sum);
